dfad64b18a1c2d5dccebb56e6919c102e20c1fa3
Tathagata Chakraborty <tathagata1805@gmail.com>
2023-05-17 19:31:59 +0530

    fixes

--- server.js
+++ server.js
@@ -3,28 +3,13 @@ const dotenv = require("dotenv");
 const app = express();
 const port = process.env.PORT || 5000;
 const dbConnection = require("./db");
+
+dotenv.config();
 app.use(express.json());
 
 app.use("/api/cars/", require("./routes/carsRoute"));
 app.use("/api/users/", require("./routes/usersRoute"));
 app.use("/api/bookings/", require("./routes/bookingsRoute"));
-
-const path = require("path");
-
-// if (process.env.NODE_ENV === "production") {
-//   const path = require("path");
-//   app.use(express.static(path.resolve(__dirname, "client", "build")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(
-//       path.resolve(__dirname, "client", "build", "index.html"),
-//       function (err) {
-//         if (err) {
-//           res.status(500).send(err);
-//         }
-//       }
-//     );
-//   });
-// }
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
